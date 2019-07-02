17def801c59ca2ff9de5ed5f6c7d437f0b741a54
Neil Floyd <neilfloyd99@gmail.com>
2019-07-02 00:17:29 -0400

    first commit: attempting to solve question in codeflow discord javascript channel

---
+++ main.js
@@ -0,0 +1,30 @@
+function largest(arr) {
+    let str = arr.toString();
+    //console.log(str);
+    let newArr = str.split(',');
+    //console.log(newArr);
+    let sorted = newArr.sort();
+    //console.log(sorted);
+    let numArr = [];
+    for(let i = 0; i < sorted.length / 2; i++) {
+        numArr.push(sorted[i]);
+    }
+    let largestNum = numArr[numArr.length - 1];
+    let newLargestNum = parseInt(largestNum);
+    //console.log(typeof newLargestNum);
+    
+    let result = [];
+    for(let y = 0; y < arr.length; y++) {
+            if(arr[y][1] == newLargestNum) {
+                result.push(arr[y]);
+            }
+        }
+        console.log(result);
+        return result;
+    }
+    
+
+
+
+largest([["won",5],["Coderunner",3],["Subi",5]]);
+largest([["won",5],["Coderunner",3],["Subi",5],["ksee", 5]]);
