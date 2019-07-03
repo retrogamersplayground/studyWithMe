a09790e42379bd50aa8cbced42d6157aed900dd3
Neil Floyd <neilfloyd99@gmail.com>
2019-07-03 11:43:05 -0400

    Study with me livestream 07/03/2019: Correcting code from the 07/01/2019 Study with me livestream

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-//live stream 07/01/2019
-
+// Study with me live stream 07/01/2019
+/*
 function largest(arr) {
     let str = arr.toString();
     //console.log(str);
@@ -30,3 +30,37 @@ function largest(arr) {
 
 largest([["won",5],["Coderunner",3],["Subi",5]]);
 largest([["won",5],["Coderunner",3],["Subi",5],["ksee", 5]]);
+*/
+
+
+//Study with me live stream 07/03/2019
+//Correcting the code from the stream on 07/01/2019
+
+function largest(arr) {
+    let newArr = [];
+    for(let x = 0; x < arr.length; x++) {
+        newArr.push(arr[x][1]);
+    }
+    //console.log(newArr);
+    
+    newArr.sort(function(a,b) {
+        return a - b;
+    });
+
+    console.log(newArr);
+    let largestNum = newArr[newArr.length - 1];
+    console.log(largestNum);
+
+    let result = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i][1] === largestNum) {
+            result.push(arr[i]);
+        }
+    }
+
+    console.log(result);
+}
+
+
+largest([["won",5],["Coderunner",3],["Subi",5]]);
+largest([["won",5],["Coderunner",3],["Subi",55],["ksee", 5]]);
